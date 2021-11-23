a84dd7954b5c36e8ef7025a8e989da18fe75047a
jexio <ptjexio@gmail.com>
2021-11-23 16:20:28 +0700

    fix(core): fix __init__ file

--- fulmo/core/__init__.py
+++ fulmo/core/__init__.py
@@ -1,3 +1,2 @@
-from .datamodule import BaseDataModule  # noqa: F401
-from .datamodule import BaseDataModuleParameters  # noqa: F401
+from .datamodule import BaseDataModule, BaseDataModuleParameters  # noqa: F401
 from .module import BaseModule  # noqa: F401
